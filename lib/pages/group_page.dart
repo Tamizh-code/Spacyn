931ad507bc626f1a9ea7803568b4f13f891872da
heartdanish <danishequbal74@gmail.com>
2025-09-10 15:44:33 +0530

    i updated all the skeliton of the page

---
+++ lib/pages/group_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GroupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Group Page")),
+      body: const Center(
+        child: Text("This is the Group Page"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/posts_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PostsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Posts")),
+      body: const Center(
+        child: Text("This is the Posts Page"),
+      ),
+    );
+  }
+}
